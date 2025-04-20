9ed5b3a16293c050e58404fbfe7a09b209de6c4b
keyobs <elodiekoob@gmail.com>
2025-04-20 13:46:12 +0200

    feature = generate planner component

---
+++ src/app/features/planner/planner.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PlannerComponent } from './planner.component';
+
+export const PLANNER_ROUTES: Routes = [
+  {
+    path: '',
+    component: PlannerComponent
+  }
+];
